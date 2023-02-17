17c4d3de6047525a42af0b87507b75c08ece6373
Litttle-panda <117175961+Litttle-panda@users.noreply.github.com>
2023-02-17 15:29:53 +0000

    moved questions to seperate file, got README to be created - need to work on markdown file to fill out as returning as undefined

--- index.js
+++ index.js
@@ -2,76 +2,25 @@ const fs = require("fs");
 const path = require('path');
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
+const questions = require("./utils/questions")
 
-// array of questions for user
-const questions = [
-    {
-        type: "input",
-        message: "What Is the Title of your project?",
-        name: "Title",
-    },
-    {
-        type: "input",
-        message: "How would you describe your project?",
-        name: "Description",
-    },
-    {
-        type: "input",
-        message: "Can you list a table of contents?",
-        name: "Table of Contents",
-    },
-    {
-        type: "input",
-        message: "How do you install your project?",
-        name: "Installation",
-    },
-    {
-        type: "input",
-        message: "How do you use your project?",
-        name: "Usage",
-    },
-    {
-        type: "checkbox",
-        message: "What Licence does you project have?",
-        name: "Licence",
-        choices: [
-            'MIT', 
-            'Apache', 
-            'Microsoft Public Licence', 
-            'Eclipse Public Licence', 
-            'ISC', 
-            'Mozilla Public Licence 2.0', 
-            'Open Software Licence 3.0',
-        ]
-    },
-    {
-        type: "input",
-        message: "What are the contributing instructions",
-        name: "Contributing",
-    },
-    {
-        type: "input",
-        message: "What are the tests used",
-        name: "Tests",
-    },
-    {
-        type: "input",
-        message: "Questions",
-        name: "Questions",
-    },
-    
-];
 
 // function to write README file
 function writeToFile(fileName, data) {
-    
-
-}
+            fs.writeFile(fileName, JSON.stringify(data, null, '\t'), (err) =>
+                err ? console.error(err) : console.log('README successfully written!'))
+};
 
 // function to initialize program
 function init() {
-
-}
+    inquirer
+        .prompt(questions).then((data) => {
+            const filename = `${data.title}-README.md`;
+            writeToFile(filename, generateMarkdown(data))
+        });
+    
+};
 
 // function call to initialize program
 init();
+
