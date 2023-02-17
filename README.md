a7983f79fd9dddc1b74f3f918d7ad0c17f6cff58
Litttle-panda <117175961+Litttle-panda@users.noreply.github.com>
2023-02-17 14:49:38 +0000

    added questions to index.js

--- README.md
+++
@@ -1,2 +0,0 @@
-# README-Generator
-A command-line application that dynamically creates a professional README.md from user inputs.
--- index.js
+++ index.js
@@ -5,11 +5,67 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
-
+    {
+        type: "input",
+        message: "What Is the Title of your project?",
+        name: "Title",
+    },
+    {
+        type: "input",
+        message: "How would you describe your project?",
+        name: "Description",
+    },
+    {
+        type: "input",
+        message: "Can you list a table of contents?",
+        name: "Table of Contents",
+    },
+    {
+        type: "input",
+        message: "How do you install your project?",
+        name: "Installation",
+    },
+    {
+        type: "input",
+        message: "How do you use your project?",
+        name: "Usage",
+    },
+    {
+        type: "checkbox",
+        message: "What Licence does you project have?",
+        name: "Licence",
+        choices: [
+            'MIT', 
+            'Apache', 
+            'Microsoft Public Licence', 
+            'Eclipse Public Licence', 
+            'ISC', 
+            'Mozilla Public Licence 2.0', 
+            'Open Software Licence 3.0',
+        ]
+    },
+    {
+        type: "input",
+        message: "What are the contributing instructions",
+        name: "Contributing",
+    },
+    {
+        type: "input",
+        message: "What are the tests used",
+        name: "Tests",
+    },
+    {
+        type: "input",
+        message: "Questions",
+        name: "Questions",
+    },
+    
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
+    
+
 }
 
 // function to initialize program
